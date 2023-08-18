c555eab69b6c7706a48f12a08519a50086a54ced
Neo <ramoroka.neo@gmail.com>
2023-08-18 11:38:55 +0200

    main

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import React from 'react';
 function App() {
   return (
     <form>
+    <div>
+    <header/>
+    <label>React App & Firebase</label>
+    </div>
+    <br/>
       <label>Amethyst
       <br/>
       <input type='text' />
       <br/>
-      <button>save</button>
+      <button>Save</button>
       <br/>
       </label>
       <br/>
       <label>Onyx</label>
       <br/>
       <input type='text' />
+      <br/>
+      <button>Edit</button>
+      <br/>
+      <label>Topaz</label>
+      <br/>
+      <input type='text'></input>
+      <br/>
+      <button>Add</button>
     </form>
   );
 }
