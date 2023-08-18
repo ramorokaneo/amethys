45cca42ec646c70f3a72cd7f992e7feb15a8a888
Neo <ramoroka.neo@gmail.com>
2023-08-18 16:02:12 +0200

    main

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <button>save</button>
-        <label>Amethyst</label>
-        <form>
-          <label>Onyx</label>
-          <input type='text' />
-          <button>Edit</button>
-        </form>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      <header/>
+      <label>Gem Stones</label>
+      <br/>
+      <br/>
+      <label>Amethyst
+      <br/>
+      <input type='text' />
+      </label>
+      <button>add</button>
+      <button>edit</button>
+      <button>save</button>
+      <br/>
+      <label>Onyx
+      <br/>
+      <input type='text' />
+      </label>
+      <button>add</button>
+      <button>edit</button>
+      <button>save</button>
+      <br/>
+      <label>Topaz
+      <br/>
+      <input type='text' />
+      </label>
+      <button>add</button>
+      <button>edit</button>
+      <button>save</button>
+    </form>
+  )
 }
 
 export default App;
